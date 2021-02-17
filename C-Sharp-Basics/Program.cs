564126fd708ffa563ebff9eb84dfaa19207a13a1
hrid0ybd <hridoyahmed11197@gmail.com>
2021-02-17 11:35:15 +0600

    Arrays

--- C-Sharp-Basics/Program.cs
+++ C-Sharp-Basics/Program.cs
@@ -1,37 +1,56 @@
 ﻿using System;
 
+// Use System.Linq
+using System.Linq;
+
 namespace C_Sharp_Basics
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Switch Case
-            int day = 5;
-            switch (day) 
+            // Arrays
+            string [] cars = new string[3];
+            cars[0] = "Volvo";
+            cars[1] = "Tesla";
+            cars[2] = "Ford";
+
+            Console.WriteLine(cars[2]); //Ford
+
+            int [] myNum = {100, 20, 300, 40};
+
+            Console.WriteLine(myNum[3]);
+
+            // Length
+            Console.WriteLine(myNum.Length);
+
+            // Loop Through Array
+            for (int i = 0; i < myNum.Length; i++)
             {
-            case 1:
-                Console.WriteLine("Saturday");
-                break;
-            case 2:
-                Console.WriteLine("Sunday");
-                break;
-            case 3:
-                Console.WriteLine("Monday");
-                break;
-            case 4:
-                Console.WriteLine("Tuesday");
-                break;
-            case 5:
-                Console.WriteLine("Wednesday");
-                break;
-            case 6:
-                Console.WriteLine("Thursday");
-                break;
-            case 7:
-                Console.WriteLine("Friday");
-                break;
+                Console.WriteLine(myNum[i]);
             }
+
+            // Foreach
+            foreach(string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            // Sort Arrays
+            Array.Sort(myNum); // 20, 40, 100, 300
+            Console.WriteLine(myNum[0]);
+
+            for (int i=0; i<myNum.Length; i++)
+            {
+                Console.WriteLine(myNum[i]);
+            }
+
+            // Max, Min, Sum
+            Console.WriteLine(myNum.Max());
+            Console.WriteLine(myNum.Min());
+            Console.WriteLine(myNum.Sum());
+
         }
     }
 }
