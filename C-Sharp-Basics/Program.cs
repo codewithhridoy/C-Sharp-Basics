8f36fa904bc0fb9ff7086657037d9faa271f701c
hrid0ybd <hridoyahmed11197@gmail.com>
2021-02-21 10:36:36 +0600

    Property

--- C-Sharp-Basics/Program.cs
+++ C-Sharp-Basics/Program.cs
@@ -1,27 +1,24 @@
-﻿    using System;  
-       public class Employee  
-        {  
-            public int id;   
-            public String name;  
-            public float salary;  
-            public Employee(int i, String n,float s)  
-            {  
-                id = i;  
-                name = n;  
-                salary = s;  
-            }  
-            public void display()  
-            {  
-                Console.WriteLine(id + " " + name+" "+salary);  
-            }  
-       }  
-       class TestEmployee{  
-           public static void Main(string[] args)  
-            {  
-                Employee e1 = new Employee(101, "Hridoy", 890000);  
-                Employee e2 = new Employee(102, "Ahmed", 490000);  
-                e1.display();  
-                e2.display();  
-      
-            }  
-        }  
+﻿using System;
+using System.Linq;
+
+namespace C_Sharp_Basics
+{
+    class Person
+    {
+        // property
+        public string Name
+        { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person aPerson = new Person();
+            aPerson.Name = "Hridoy";
+
+            Console.WriteLine(aPerson.Name);
+
+        }
+            
+    }
+}
