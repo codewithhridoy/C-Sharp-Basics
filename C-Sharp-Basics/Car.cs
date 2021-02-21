a916600b361c1921e8b6d579ccc8173203faa778
hrid0ybd <hridoyahmed11197@gmail.com>
2021-02-21 10:07:11 +0600

    Class Basics

---
+++ C-Sharp-Basics/Car.cs
@@ -0,0 +1,13 @@
+using System;
+class Car
+{
+    public string model;
+    public string color;
+    public int year;
+    public void speedOfCar()
+    {
+        Console.WriteLine("Speed is great");
+    }
+
+
+}
