c2e756d66098410a0f9522388cc8c3437479077d
hrid0ybd <hridoyahmed11197@gmail.com>
2021-02-21 10:21:24 +0600

    Constructor

--- C-Sharp-Basics/Program.cs
+++ C-Sharp-Basics/Program.cs
@@ -1,19 +1,27 @@
-﻿using System;
-using System.Linq;
-
-namespace C_Sharp_Basics
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Car Ford = new Car();
-            Ford.model = "Mustang";
-            Ford.color = "Black";
-            Ford.year = 1969;
-
-            Console.WriteLine(Ford.model);
-        }
-            
-    }
-}
+﻿    using System;  
+       public class Employee  
+        {  
+            public int id;   
+            public String name;  
+            public float salary;  
+            public Employee(int i, String n,float s)  
+            {  
+                id = i;  
+                name = n;  
+                salary = s;  
+            }  
+            public void display()  
+            {  
+                Console.WriteLine(id + " " + name+" "+salary);  
+            }  
+       }  
+       class TestEmployee{  
+           public static void Main(string[] args)  
+            {  
+                Employee e1 = new Employee(101, "Hridoy", 890000);  
+                Employee e2 = new Employee(102, "Ahmed", 490000);  
+                e1.display();  
+                e2.display();  
+      
+            }  
+        }  
